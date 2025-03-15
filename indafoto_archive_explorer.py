a6b777d7d6ff090f789bca4e3a24e4f2c1fb373d
bdamokos <163609735+bdamokos@users.noreply.github.com>
2025-03-15 16:05:42 +0100

    future development

---
+++ indafoto_archive_explorer.py
@@ -0,0 +1,3 @@
+#TODO: We should have a way to browse the preserved images and see the data we have on them
+# We should be able to recreate a view per user and per gallery
+# We should be able to mark images worth preserving and save this info to database with a reference to the image (e.g. the ID of the image)
